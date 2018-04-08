cb31710888b0d1ab35254ba8ed555ae27b89969d
hexin <hexin.work@outlook.com>
2018-04-08 19:40:54 +0800

    according to review, change: 1. edit TODO comments with description of your test 2. use jQuery hasClass to decide whether element has that class 3. use jQuery parent-child relationship syntax to get all child under parent

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,8 +27,8 @@ $(function () {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
+        /* Loops through each feed in the allFeeds object 
+         * and ensures it has a URL defined
          * and that the URL is not empty.
          */
         it('has URL defined and is not empty', function () {
@@ -38,8 +38,8 @@ $(function () {
             }
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
+        /* Loops through each feed in the allFeeds object 
+         * and ensures it has a name defined
          * and that the name is not empty.
          */
         it('has name defined and is not empty', function () {
@@ -51,33 +51,32 @@ $(function () {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Test suite test whethear "The menu" element behave what we expect */
     describe('The menu', function () {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
+        /* The menu element is hidden by default. 
+         * You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
         it('is hidden by default', function () {
-            expect(document.body.className).toBe('menu-hidden');
+            expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-        /* TODO: Write a test that ensures the menu changes
-         * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
+        /* The menu changes visibility when the menu icon is clicked. 
+         * This test should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
         it('changes visibility when the menu icon is clicked.', function () {
             $('.menu-icon-link').trigger("click");
-            expect(document.body.className).toBe('');
+            expect($('body').hasClass('menu-hidden')).toBe(false);
             $('.menu-icon-link').trigger("click");
-            expect(document.body.className).toBe('menu-hidden');
+            expect($('body').hasClass('menu-hidden')).toBe(true);
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Test suite test "Initial Entries" is loaded */
     describe('Initial Entries', function () {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -88,16 +87,16 @@ $(function () {
         });
 
         it('the loadFeed function is called and completes its work.', function (done) {
-            let entryCount = $('.feed').find('.entry').length;
+            let entryCount = $('.feed .entry').length;
             expect(entryCount).toBeGreaterThan(0);
             done();
         });
     });
 
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Test suite test "New Feed Selection" is work and fetch different website context */
     describe('New Feed Selection', function () {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
